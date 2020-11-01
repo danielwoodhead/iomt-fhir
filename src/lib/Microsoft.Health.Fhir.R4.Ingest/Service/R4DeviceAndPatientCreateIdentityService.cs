bfcdeff7992060d5e38716f44cd175caeb122d97
Daniel Woodhead <danny2311@hotmail.com>
2020-11-01 15:01:32 +0000

    debugging

--- src/lib/Microsoft.Health.Fhir.R4.Ingest/Service/R4DeviceAndPatientCreateIdentityService.cs
+++ src/lib/Microsoft.Health.Fhir.R4.Ingest/Service/R4DeviceAndPatientCreateIdentityService.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using EnsureThat;
@@ -77,6 +78,7 @@ namespace Microsoft.Health.Fhir.Ingest.Service
                 {
                     if (ResourceIdentityOptions.UseIdentifierAsResourceId)
                     {
+                        Console.WriteLine("HELLO!!!");
                         p.Id = input.PatientId;
                     }
 
